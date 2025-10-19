6407539e7b89636219fd3dd1c3ab91847d30f991
son <thswjddn0812@gmail.com>
2025-10-19 23:16:00 +0900

    리듬게임 중간완료

--- game/index.js
+++ game/index.js
@@ -1,31 +1,93 @@
-let size = 200;
-const minSize = 50;
-const speed = 2;
-const border = document.querySelector("#circleBorder");
-const circle = document.querySelector("#circle");
+const gameArea = document.querySelector("#gameArea");
+const scoreBoard = document.querySelector("#scoreBoard");
+const timeBoard = document.querySelector("#timeBoard");
+const startBtn = document.querySelector("#startBtn");
 
-function animate() {
-  size -= speed;
-  if (size <= minSize) size = 200;
-  border.style.width = size + "px";
-  border.style.height = size + "px";
-  border.style.left = `calc(50% - ${size / 2}px)`;
-  border.style.top = `calc(50% - ${size / 2}px)`;
-  requestAnimationFrame(animate);
-}
-circle.addEventListener("click", () => {
-  if (size >= 50 && size <= 70) {
-    console.log("포풱트");
-    circle.style.display = "None";
-    border.style.display = "None";
-  } else {
-    console.log("ㄲㅂ");
-    circle.style.display = "None";
-    border.style.display = "None";
-  }
+startBtn.addEventListener("click", () => {
+  startGame();
 });
-animate();
-
-//   const borderRect = border.getBoundingClientRect();
-//   const circleRect = circle.getBoundingClientRect();
-//   const diff = Math.abs(borderRect.width - circleRect.width);
+startGame(() => {
+  const startScore = 0;
+  let score = 0;
+  const startTime = 15;
+  let timeCount = startTime;
+  startBtn.style.display = "none";
+  timerInterval = setInterval(() => {
+    timeCount--;
+    if (timeCount == 0) {
+      clearInterval(timerInterval);
+      alert(`게임 종료 점수는 ${score}`);
+    }
+  }, 1000);
+  gameInterval = setInterval(spawnCircle, 700);
+});
+spawnCircle(() => {
+  const circle = document.createElement("div");
+  const border = document.createElement("div");
+  gameArea.appendChild(circle);
+  gameArea.appendChild(border);
+  const size = 50;
+  const rect = gameArea.getBoundingClientRect();
+  const x = Math.random() * (rect.width - size);
+  const y = Math.random() * (rect.height - size);
+  circle.style.width = border.style.width = `${size}px`;
+  circle.style.height = border.style.height = `${size}px`;
+  circle.style.left = border.style.left = `${x}px`;
+  circle.style.top = border.style.top = `${y}px`;
+  //
+  let scale = 2;
+  const downSpeed = 0.02;
+  const animation = setInterval(() => {
+    scale -= downSpeed;
+    border.style.transform = `scale(${scale})`;
+    if (scale <= 1) {
+      clearInterval(animation);
+      gameArea.removeChild(border);
+      gameArea.removeChild(circle);
+    }
+  });
+  circle.addEventListener("click", () => {
+    if (scale >= 1 && scale < 1.2) {
+      const perfectPoint = 200;
+      score += perfectPoint;
+      scoreBoard.innerText = `Score:${score}`;
+      const perfect = document.createElement("div");
+      perfect.innerText = "perfect";
+      gameArea.appendChild(perfect);
+      setTimeout(() => perfect.remove(), 700);
+      perfect.style.position = "absolute";
+      perfect.style.left = `${x + 60}px`;
+      perfect.style.top = `${y}px`;
+      gameArea.removeChild(border);
+      gameArea.removeChild(circle);
+      //
+    } else if (scale >= 1.2 && scale < 1.5) {
+      const goodPoint = 100;
+      score += goodPoint;
+      scoreBoard.innerText = `Score:${score}`;
+      const good = document.createElement("div");
+      good.innerText = "good";
+      gameArea.appendChild(good);
+      setTimeout(() => good.remove(), 700);
+      good.style.position = "absolute";
+      good.style.left = `${x + 60}px`;
+      good.style.top = `${y}px`;
+      gameArea.removeChild(border);
+      gameArea.removeChild(circle);
+      //
+    } else if (scale <= 1.8 && scale >= 1.7) {
+      const missPoint = -100;
+      score += missPoint;
+      scoreBoard.innerText = `Score:${score}`;
+      const miss = document.createElement("div");
+      miss.innerText = "Miss";
+      gameArea.appendChild(miss);
+      setTimeout(() => miss.remove(), 700);
+      miss.style.position = "absolute";
+      miss.style.left = `${x + 60}px`;
+      miss.style.top = `${y}px`;
+      gameArea.removeChild(border);
+      gameArea.removeChild(circle);
+    }
+  });
+});
